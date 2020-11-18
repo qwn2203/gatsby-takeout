df6292d43fff7993f6cf4432ae38ddc563e1eeda
Alma Ayaquica <yogo060@gmail.com>
2020-11-18 11:43:12 -0600

    form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,47 @@
 import React from "react"
 import Layout from "../components/layout"
 import LeafletMap from "../components/leafletmap"
-import { DatePicker } from "@material-ui/pickers";
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
-const IndexPage = () => (
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function IndexPage(){
+  const classes = useStyles();
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
   <Layout>
     <section style={{ "margin": "0.5rem 1rem" }}>
-      
+    <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
     </section>
 
     {typeof window !== 'undefined' &&
@@ -19,5 +54,4 @@ const IndexPage = () => (
 
   </Layout>
   )
-
-  export default IndexPage
+}
