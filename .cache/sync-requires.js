704ee6736331376708b26eb16bc84209ad2ed146
Alma Ayaquica <yogo060@gmail.com>
2020-11-18 10:57:31 -0600

    init

---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("D:\\Documents\\TEC\\9no - AGO-DIC 20\\web\\gatsby-takeout\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("D:\\Documents\\TEC\\9no - AGO-DIC 20\\web\\gatsby-takeout\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("D:\\Documents\\TEC\\9no - AGO-DIC 20\\web\\gatsby-takeout\\src\\pages\\index.js")))
+}
+
