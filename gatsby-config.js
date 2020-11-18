305668e189503f17d649028eec1d9f741b8300ba
Alma Ayaquica <yogo060@gmail.com>
2020-11-18 10:48:39 -0600

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   pathPrefix: `gatsby-geo-simple-map`,
   siteMetadata: {
     title: `Simple Map`,
-    description: `Demonstrating how to display a simple map with a marker and pop-up with Gatsby`,
-    author: `@andrewl`,
+    description: ``,
+    author: `@almaA`,
   },
   plugins: [
     `gatsby-plugin-react-leaflet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,7 @@ import LeafletMap from "../components/leafletmap"
 const IndexPage = () => (
   <Layout>
     <section style={{ "margin": "0.5rem 1rem" }}>
-      <p>Demonstrating how to display a simple map with a marker and pop-up.</p>
-      <p>For more information see <a href="https://www.andrewl.net/article/gatsby-geo-simple-map">https://www.andrewl.net/article/gatsby-geo-simple-map</a></p>
+      
     </section>
 
     {typeof window !== 'undefined' &&
